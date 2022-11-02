03e29aa774e5a770b1dbfc00d800dd63b06194d2
hosein.nzd <hosein.nzd26@gmail.com>
2022-11-02 17:33:00 +0330

    set navigationComponent with bottomNavigation

---
+++ app/src/main/java/com/nzd/megatask/view/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.nzd.megatask.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.nzd.megatask.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //bottomNavigation
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(1).isEnabled = false
+
+        //setBottomNavigation with fragments
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(bottomNavigationView , navController)
+    }
+}
